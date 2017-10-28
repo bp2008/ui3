c9bad68456ea67679f906ad5de6d441973d2bd72
Brian <bp2008@users.noreply.github.com>
2017-10-27 18:08:25 -0600

    Removed unneeded code from the audio playback efforts.

--- ui3/ui3.js
+++ ui3/ui3.js
@@ -10123,50 +10123,6 @@ function MuLawDecoder()
 		return decoded;
 	}
 }
-// TODO: Remove this, as it is probably not needed.
-///////////////////////////////////////////////////////////////
-// PCM16 to Wav Encoder ///////////////////////////////////////
-///////////////////////////////////////////////////////////////
-//function Pcm16ToWav(pcm16, channels, sampleRate)
-//{
-//	/// <summary>Encodes a Int16Array of raw PCM as a complete wav and returns the result in an ArrayBuffer.</summary>
-//	var header = new ArrayBuffer(44);
-//	var data = pcm16.buffer;
-
-//	var d = new DataView(header);
-
-//	d.setUint8(0, 'R'.charCodeAt(0));
-//	d.setUint8(1, 'I'.charCodeAt(0));
-//	d.setUint8(2, 'F'.charCodeAt(0));
-//	d.setUint8(3, 'F'.charCodeAt(0));
-
-//	d.setUint32(4, data.byteLength / 2 + 44, true);
-
-//	d.setUint8(8, 'W'.charCodeAt(0));
-//	d.setUint8(9, 'A'.charCodeAt(0));
-//	d.setUint8(10, 'V'.charCodeAt(0));
-//	d.setUint8(11, 'E'.charCodeAt(0));
-//	d.setUint8(12, 'f'.charCodeAt(0));
-//	d.setUint8(13, 'm'.charCodeAt(0));
-//	d.setUint8(14, 't'.charCodeAt(0));
-//	d.setUint8(15, ' '.charCodeAt(0));
-
-//	d.setUint32(16, 16, true);
-//	d.setUint16(20, 1, true);
-//	d.setUint16(22, numberOfChannels, true);
-//	d.setUint32(24, sampleRate, true);
-//	d.setUint32(28, sampleRate * 1 * 2);
-//	d.setUint16(32, numberOfChannels * 2);
-//	d.setUint16(34, 16, true);
-
-//	d.setUint8(36, 'd'.charCodeAt(0));
-//	d.setUint8(37, 'a'.charCodeAt(0));
-//	d.setUint8(38, 't'.charCodeAt(0));
-//	d.setUint8(39, 'a'.charCodeAt(0));
-//	d.setUint32(40, data.byteLength, true);
-
-//	return ConcatenateArrayBuffers(header, data);
-//}
 ///////////////////////////////////////////////////////////////
 // Audio Filter (silences crackle at frame boundaries) ////////
 ///////////////////////////////////////////////////////////////
@@ -13492,12 +13448,4 @@ function GetServerTimeOffset()
 	var localOffsetMs = new Date().getTimezoneOffset() * 60000;
 	var serverOffsetMs = serverTimeZoneOffsetMs;
 	return localOffsetMs - serverOffsetMs;
-}
-// TODO: Remove this, as it is probably not needed.
-function ConcatenateArrayBuffers(a, b)
-{
-	var c = new Uint8Array(a.byteLength + b.byteLength);
-	c.set(new Uint8Array(a), 0);
-	c.set(new Uint8Array(b), a.byteLength);
-	return c.buffer;
 }
