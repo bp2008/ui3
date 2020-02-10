f9cc84b70fbc4c73ca1f572b4a224c12ed2cd556
bp2008 <bp2008@users.noreply.github.com>
2020-02-10 09:24:42 -0700

    Updated Queue class: * Consumed items from the backing array are now unreferenced immediately (previously, only when the array's free space was trimmed). * The backing array is trimmed less aggressively, reducing CPU usage slightly at the cost of a very small amount of wasted memory. * Simplified the "find" function and fixed a bug which caused it to not iterate over all items.  Fortunately this function was unused in UI3 anyway.

--- ui3/libs-src/Queue.src.js
+++ ui3/libs-src/Queue.src.js
@@ -11,6 +11,8 @@ http://creativecommons.org/publicdomain/zero/1.0/legalcode
 
 replaceFront method added by bp2008, Sept 2017
 
+other modifications made by bp2008
+
 */
 
 /* Creates a new queue. A queue is a first-in-first-out (FIFO) data structure -
@@ -18,46 +20,55 @@ replaceFront method added by bp2008, Sept 2017
  */
 function Queue()
 {
-
 	// initialise the queue and offset
 	var queue = [];
 	var offset = 0;
 
-	// Returns the length of the queue.
+	/**
+	 * Returns the length of the queue.
+	 * @returns {Number} the length of the queue
+	 */
 	this.getLength = function ()
 	{
-		return (queue.length - offset);
-	}
+		return queue.length - offset;
+	};
 
-	// Returns true if the queue is empty, and false otherwise.
+	/**
+	 * Returns true if the queue is empty, and false otherwise.
+	 * @returns {Boolean} true if the queue is empty
+	 */
 	this.isEmpty = function ()
 	{
-		return (queue.length == 0);
-	}
+		return queue.length === offset;
+	};
 
-	/* Enqueues the specified item. The parameter is:
-	 *
-	 * item - the item to enqueue
+	/**
+	 * Enqueues the specified item.
+	 * @param {Any} item the item to enqueue
 	 */
 	this.enqueue = function (item)
 	{
 		queue.push(item);
-	}
+	};
 
-	/* Dequeues an item and returns it. If the queue is empty, the value
-	 * 'undefined' is returned.
+	/**
+	 * Dequeues an item and returns it. If the queue is empty, the value 'undefined' is returned.
+	 * @returns {Any} The item which was at the front of the queue, or 'undefined'.
 	 */
 	this.dequeue = function ()
 	{
 
 		// if the queue is empty, return immediately
-		if (queue.length == 0) return undefined;
+		if (queue.length === 0) return undefined;
 
 		// store the item at the front of the queue
 		var item = queue[offset];
 
+		// Remove the item from the queue so it can be garbage collected.
+		queue[offset] = undefined;
+
 		// increment the offset and remove the free space if necessary
-		if (++offset * 2 >= queue.length)
+		if (queue.length > 16 && ++offset * 2 >= queue.length)
 		{
 			queue = queue.slice(offset);
 			offset = 0;
@@ -66,18 +77,20 @@ function Queue()
 		// return the dequeued item
 		return item;
 
-	}
+	};
 
-	/* Returns the item at the front of the queue (without dequeuing it). If the
-	 * queue is empty then undefined is returned.
+	/**
+	 * Returns the item at the front of the queue (without dequeuing it). If the queue is empty then undefined is returned.
+	 * @returns {Any} The item which is at the front of the queue, or 'undefined'.
 	 */
 	this.peek = function ()
 	{
 		return (queue.length > 0 ? queue[offset] : undefined);
-	}
+	};
 
-	/* Replaces the item at the front of the queue with a different item. If the
-	 * queue is empty then this method behaves like [enqueue].
+	/**
+	 * Replaces the item at the front of the queue with a different item. If the queue is empty then this method behaves like [enqueue].
+	 * @param {Any} newFront the item to put at the front of the queue
 	 */
 	this.replaceFront = function (newFront)
 	{
@@ -85,27 +98,31 @@ function Queue()
 			queue[offset] = newFront;
 		else
 			queue.push(newFront);
-	}
+	};
 
+	/**
+	 * Returns an array containing all the items in the queue, in the order they would have been dequeued in.
+	 * @returns {Array} Returns an array containing all the items in the queue, in the order they would have been dequeued in.
+	 */
 	this.toArray = function ()
 	{
-		var len = (queue.length - offset);
+		var len = queue.length - offset;
 		var arr = new Array(len);
-		var n = 0;
-		for (var i = offset; i < queue.length; i++)
-			arr[n++] = queue[i];
+		for (var i = offset, n = 0; i < queue.length; i++, n++)
+			arr[n] = queue[i];
 		return arr;
-	}
+	};
+
+	/**
+	 * Returns the first item that causes the specified predicate to return true.
+	 * @param {Function} where A function to which each item from the queue is passed. The function should return true when the desired item is passed in.
+	 * @returns {Any} Returns the first item that causes the specified predicate to return true.
+	 */
 	this.find = function (where)
 	{
-		var len = (queue.length - offset);
-		for (var i = 0, n = offset; i < len; i++ , n++)
-		{
-			if (n >= len)
-				n = 0;
-			if (where(queue[n]))
-				return queue[n];
-		}
-	}
-
+		for (var i = offset; i < queue.length; i++)
+			if (where(queue[i]))
+				return queue[i];
+		return undefined;
+	};
 }
