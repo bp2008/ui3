f3de33e944bb7128e1e6de44c8c7332247f9ec61
bp2008 <bp2008@users.noreply.github.com>
2023-09-09 16:05:13 -0600

    Help file revisions and redesign so that the home page does not have any sections hidden, to enable `CTRL + F` finding to access the entire help contents.

--- ui3/help/js/help.js
+++ ui3/help/js/help.js
@@ -42,3 +42,105 @@ function RemoveUrlParams()
 	}
 	return location.origin + location.pathname + s + location.hash;
 }
+
+
+$(function ()
+{
+	var imgs = document.getElementsByTagName("img");
+	for (var i = 0; i < imgs.length; i++)
+	{
+		var src = imgs[i].getAttribute("mysrc");
+		if (src)
+			imgs[i].src = src + (navigator.cookieEnabled ? "" : "?session=" + local_bi_session);
+	}
+	try
+	{
+		var profileData = JSON.parse(localStorage.ui3_streamingProfileArray);
+		for (var i = 0; i < profileData.length; i++)
+		{
+			var row = $('<tr></tr>');
+			var cell_name = $('<td></td>').text(profileData[i].name);
+			var cell_nameEnc = $('<td></td>').append($('<code></code>').text(encodeURIComponent(profileData[i].name)));
+			row.append(cell_name);
+			row.append(cell_nameEnc);
+			$("#streamingProfileTbody").append(row);
+		}
+		$("#streamingProfileListParagraph").show();
+	}
+	catch (ex)
+	{
+		console.log("Unable to load streaming profiles. Local storage is probably not enabled.");
+		console.error(ex);
+	}
+
+	try
+	{
+		// Set the current time minus one minute in the Timeline example link on the URL Parameters page
+		$("#timelineLink").attr("href", $("#timelineLink").attr("href") + (Date.now() - 60000));
+		$("#timelineLink").text($("#timelineLink").text() + (Date.now() - 60000));
+	}
+	catch (ex)
+	{
+		console.log("Unable to set timelineLink.");
+		console.error(ex);
+	}
+
+	function GetFirstHeading(ele)
+	{
+		for (var i = 1; i <= 9; i++)
+		{
+			var h = ele.find("h" + i).first();
+			if (h.length)
+				return h;
+		}
+		return null;
+	}
+
+	try
+	{
+		// Copy all navbar links to a list on the Home page.
+		var tabPanels = $('div.tab-pane');
+		var homeLinks = [];
+		var allTabPanes = [];
+		for (var i = 0; i < tabPanels.length; i++)
+		{
+			var tabPanel = tabPanels.eq(i);
+			allTabPanes.push(tabPanel);
+			var firstHeading = GetFirstHeading(tabPanel);
+			if (firstHeading)
+			{
+				var id = tabPanel.attr('id');
+				if (id && id !== '' && id !== 'home' && id !== 'template')
+				{
+					var dst = $('<a class="list-group-item"></a>');
+					dst.attr('href', "#" + id);
+					var text = firstHeading.text().trim();
+					dst.text(text);
+
+					homeLinks.push({ key: text, a: dst });
+				}
+			}
+		}
+
+		homeLinks.sort(function (a, b)
+		{
+			return a.key.localeCompare(b.key);
+		});
+
+		var homeIndex = $("#homeIndex");
+		for (var i = 0; i < homeLinks.length; i++)
+			homeIndex.append(homeLinks[i].a);
+
+		if (!$("#home").is(":hidden"))
+			$('div.tab-pane').addClass("active");
+	}
+	catch (ex)
+	{
+		console.log("Unable to create list of section links in Home page.");
+		console.error(ex);
+	}
+});
+$("#nav-home").on("shown.bs.tab", function ()
+{
+	$('div.tab-pane').addClass("active");
+});
