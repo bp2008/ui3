4f183383e1a794fa8b0f600209fc7d2dc877dbcc
bp2008 <bp2008@users.noreply.github.com>
2025-11-22 10:30:39 -0700

    Added BI 6 quality scaling awareness.  Default settings upgrade not implemented yet.

--- ui3/ui3.js
+++ ui3/ui3.js
@@ -16109,7 +16109,7 @@ function SessionManager()
 
 			var m = str.match(/q=(\d+)%/i);
 			if (m)
-				o.q = parseInt(m[1]);
+				o.q6 = parseInt(m[1]);
 
 			m = str.match(/(\d+)kbps/i);
 			if (m)
@@ -24189,6 +24189,7 @@ function getMaxGOP()
 }
 var ui3_streaming_quality_default = 50;
 var ui3_bitrate_default_kbps = 2000;
+var has_bi6_quality_scale = compareVersions(bi_version, "5.9.9.99") >= 0;
 function IsKbpsValid(kbps)
 {
 	return kbps && kbps >= 10;
@@ -24245,7 +24246,30 @@ function StreamingProfileEditor(srcProfile, profileEditedCallback)
 			AddEditorField("Rate Control", "rc", { type: "select", options: [{ value: "inherit", name: "inherit" }, { value: "vbr", name: "Variable Bit Rate" }, { value: "cbr", name: "Constant Bit Rate" }], hint: "CBR = Constant Bit Rate. VBR = Variable Bit Rate (favors a specific quality).", onChange: ReRender });
 
 		if (p.vcodec === "jpeg" || (p.vcodec === "h264" && p.rc === "vbr"))
-			AddEditorField("Quality [0-100]", "q", { min: 0, max: 100 });
+		{
+			var onQualityChange = function (e, key, $input)
+			{
+				if (has_bi6_quality_scale && p.vcodec === "h264")
+				{
+					p.q = MapBi6QualityToBi5Quality(p.q6);
+					var $crfLabel = findNextElement($input, '.crf_labels');
+					if ($crfLabel)
+					{
+						$crfLabel.text("(H.264 CRF: " + MapBi6QualityToH264CRF(p.q6) + ") (H.265 CRF: " + MapBi6QualityToH265CRF(p.q6) + ")");
+					}
+				}
+				else
+				{
+					p.q = p.q6;
+				}
+			};
+			AddEditorField("Quality [0-100]", "q6", { min: 0, max: 100, onChange: onQualityChange });
+			if (has_bi6_quality_scale && p.vcodec === "h264")
+			{
+				AddEditorField('<div class="crf_labels">...</span>', "uicomment", { type: "commentHtml" });
+				onQualityChange(null, "q6", $content.find('input').first());
+			}
+		}
 		if (p.vcodec === "h264")
 		{
 			if (p.rc === "vbr")
@@ -24278,6 +24302,14 @@ function StreamingProfileEditor(srcProfile, profileEditedCallback)
 		$saveBtn.on('click', SaveAndClose);
 		$content.append($('<div class="dialogOption_item_info"></div>').append($deleteBtn).append($saveBtn).append($cancelBtn));
 	}
+	var findNextElement = function ($element, selector)
+	{
+		var $next = $element.nextAll().find(selector).first();
+		// If not found, climb up the DOM and search forward from ancestors
+		if (!$next.length)
+			$next = $element.parents().nextAll().find(selector).first();
+		return $next.length ? $next : null;
+	}
 	var GetServerProfileString = function (i)
 	{
 		var str = "Streaming " + i;
@@ -24309,11 +24341,11 @@ function StreamingProfileEditor(srcProfile, profileEditedCallback)
 		{
 			if (p.rc === "vbr")
 			{
-				if (p.q < 0)
+				if (p.q6 < 0)
 				{
-					p.q = ui3_streaming_quality_default;
+					p.q6 = ui3_streaming_quality_default;
 					ReRender();
-					SimpleDialog.Text("A Quality value was not entered, so a default value (" + p.q + ") was assigned.");
+					SimpleDialog.Text("A Quality value was not entered, so a default value (" + p.q6 + ") was assigned.");
 					return;
 				}
 			}
@@ -24407,7 +24439,8 @@ function StreamingProfile()
 	// All the remaining options are "optional".  Values of -1 mean to inherit the argument from the server-side stream.
 	this.w = -1;
 	this.h = -1;
-	this.q = 50;
+	this.q = 50; // Blue Iris 5 Quality % (deprecated, maintained for backwards compatibility)
+	this.q6 = 68; // Blue Iris 6 Quality %.  68-70% is equivalent to 50% in BI5.
 	// Above values apply to H.264 and JPEG.
 	// Below values apply only to H.264.
 	this.rc = "vbr"; // Rate Control Method ["vbr", "cbr"]
@@ -24507,7 +24540,7 @@ function StreamingProfile()
 		{
 			if (self.rc === "vbr")
 			{
-				var q = self.q;
+				var q = self.q6;
 				if (q < 0)
 					q = ui3_streaming_quality_default;
 				q = Clamp(q, 0, 100);
@@ -24552,7 +24585,7 @@ function StreamingProfile()
 
 			if (self.fullRangeColor > 0)
 				sb.Append("&frc=").Append(self.fullRangeColor - 1);
-				
+
 			var supportedCodecs = videoPlayer.GetSupportedCodecs();
 
 			if (self.vcodecPref === "h264" && supportedCodecs.h264)
@@ -24575,8 +24608,8 @@ function StreamingProfile()
 		}
 		else
 		{
-			if (self.q >= 0)
-				sb.Append("&q=").Append(self.q);
+			if (self.q6 >= 0)
+				sb.Append("&q=").Append(self.q6);
 		}
 		return sb.ToString();
 	}
@@ -24610,8 +24643,8 @@ function StreamingProfile()
 
 		if (self.rc === "vbr")
 		{
-			if (self.q > -1)
-				sb.Append('q' + self.q + ' ');
+			if (self.q6 > -1)
+				sb.Append('q' + self.q6 + ' ');
 			if (IsKbpsValid(self.kbps))
 				sb.Append('<' + Clamp(self.kbps, 10, 100000) + ' Kbps ');
 		}
@@ -24672,7 +24705,7 @@ function JpegSnapshotArgs(loadingImg)
 	p.aClr = "#000000";
 	p.w = 7680;
 	p.h = 7680;
-	p.q = Clamp(parseInt(settings.ui3_download_snapshot_server_quality), 0, 100);
+	p.q6 = Clamp(parseInt(settings.ui3_download_snapshot_server_quality), 0, 100);
 
 	return "&decode=1" + p.GetUrlArgs(loadingImg);
 }
@@ -24740,8 +24773,8 @@ function GenericQualityHelper()
 			if (!best) best = self.FindBestProfile("h264", function (p) { return p.w < 1 || p.h < 1 ? -1 : Math.abs((p.w * p.h) - 1000000); });
 			if (!best) best = self.FindBestProfile("h264", function (p) { return p.w < 1 ? -1 : Math.abs(p.w - 1000); });
 			if (!best) best = self.FindBestProfile("h264", function (p) { return p.h < 1 ? -1 : Math.abs(p.h - 1000); });
-			// Then, one with q near 25
-			if (!best) best = self.FindBestProfile("h264", function (p) { return p.q < 0 ? -1 : Math.abs(p.q - 25); });
+			// Then, one with q6 near 30
+			if (!best) best = self.FindBestProfile("h264", function (p) { return p.q6 < 0 ? -1 : Math.abs(p.q6 - 30); });
 		}
 		// Fall back to the best jpeg profile
 		if (!best) best = self.FindBestProfile("jpeg", function (p)
@@ -24845,8 +24878,8 @@ function GenericQualityHelper()
 		var sizeToRequest = imageRenderer.GetSizeToRequest(loadingImg, profile);
 		var playerId = profile.vcodec;
 		var quality = null;
-		if (playerId == "jpeg" && profile.q)
-			quality = profile.q;
+		if (playerId == "jpeg" && profile.q6)
+			quality = profile.q6;
 		if (currentPrimaryTab !== "timeline")
 		{
 			if (playerId == "h264")
@@ -24907,6 +24940,7 @@ function GenericQualityHelper()
 		p.w = 7680;
 		p.h = 4320;
 		p.q = 50;
+		p.q6 = 68;
 		p.kbps = 16384;
 		p.gop = 1000;
 		return p;
@@ -24920,6 +24954,7 @@ function GenericQualityHelper()
 		p.w = 7680;
 		p.h = 4320;
 		p.q = 20;
+		p.q6 = 21;
 		p.kbps = 5000;
 		p.gop = 1000;
 		p.pre = 2;
@@ -24935,6 +24970,7 @@ function GenericQualityHelper()
 		p.w = 3840;
 		p.h = 2160;
 		p.q = 20;
+		p.q6 = 21;
 		p.kbps = 3000;
 		p.gop = 1000;
 		p.pre = 2;
@@ -24950,6 +24986,7 @@ function GenericQualityHelper()
 		p.w = 1920;
 		p.h = 1080;
 		p.q = 20;
+		p.q6 = 21;
 		p.kbps = 1000;
 		p.gop = 1000;
 		p.pre = 2;
@@ -24969,6 +25006,7 @@ function GenericQualityHelper()
 			p.w = 3840;
 			p.h = 2160;
 			p.q = 50;
+			p.q6 = 68;
 			p.kbps = 8192;
 			profiles.push(p);
 		}
@@ -24980,6 +25018,7 @@ function GenericQualityHelper()
 			p.w = 2560;
 			p.h = 1440;
 			p.q = 40;
+			p.q6 = 53;
 			p.kbps = 4096;
 			profiles.push(p);
 		}
@@ -24991,6 +25030,7 @@ function GenericQualityHelper()
 			p.w = 1920;
 			p.h = 1080;
 			p.q = 35;
+			p.q6 = 45;
 			p.kbps = 2048;
 			profiles.push(p);
 		}
@@ -25002,6 +25042,7 @@ function GenericQualityHelper()
 			p.w = 1280;
 			p.h = 720;
 			p.q = 35;
+			p.q6 = 45;
 			p.kbps = 1024;
 			profiles.push(p);
 		}
@@ -25013,6 +25054,7 @@ function GenericQualityHelper()
 			p.w = 856;
 			p.h = 480;
 			p.q = 30;
+			p.q6 = 37;
 			p.kbps = 456;
 			profiles.push(p);
 		}
@@ -25024,6 +25066,7 @@ function GenericQualityHelper()
 			p.w = 640;
 			p.h = 360;
 			p.q = 30;
+			p.q6 = 37;
 			p.kbps = 256;
 			profiles.push(p);
 		}
@@ -25035,6 +25078,7 @@ function GenericQualityHelper()
 			p.w = 427;
 			p.h = 240;
 			p.q = 30;
+			p.q6 = 37;
 			p.kbps = 114;
 			profiles.push(p);
 		}
@@ -25046,6 +25090,7 @@ function GenericQualityHelper()
 			p.w = 256;
 			p.h = 144;
 			p.q = 30;
+			p.q6 = 37;
 			p.kbps = 41;
 			profiles.push(p);
 		}
@@ -25058,6 +25103,7 @@ function GenericQualityHelper()
 			p.w = 99999;
 			p.h = 99999;
 			p.q = 85;
+			p.q6 = 85;
 			profiles.push(p);
 		}
 		{
@@ -25069,6 +25115,7 @@ function GenericQualityHelper()
 			p.w = 640;
 			p.h = 640;
 			p.q = 50;
+			p.q6 = 50;
 			profiles.push(p);
 		}
 		{
@@ -25080,6 +25127,7 @@ function GenericQualityHelper()
 			p.w = 320;
 			p.h = 320;
 			p.q = 20;
+			p.q6 = 20;
 			profiles.push(p);
 		}
 		return profiles;
@@ -25337,6 +25385,31 @@ function GenericQualityHelper()
 		this.RestoreDefaultProfiles();
 	self.QualityChoiceChanged(settings.ui3_streamingQuality);
 }
+///////////////////////////////////////////////////////////////
+// Streaming Quality Maps /////////////////////////////////////
+///////////////////////////////////////////////////////////////
+function MapBi6QualityToBi5Quality(bi6Quality)
+{
+	for (var i = bi6Quality; i <= 100; i++)
+	{
+		var q = bi6_to_bi5_quality_map[i.toString()];
+		if (typeof q !== "undefined")
+			return q;
+	}
+}
+function MapBi6QualityToH264CRF(bi6Quality)
+{
+	return bi6_to_h264_CRF_map[bi6Quality];
+}
+function MapBi6QualityToH265CRF(bi6Quality)
+{
+	return bi6_to_h265_CRF_map[bi6Quality];
+}
+// These maps are computed by a google docs spreadsheet.
+var bi6_to_bi5_quality_map = { '0': 6, '2': 7, '3': 8, '5': 9, '6': 10, '8': 11, '9': 12, '11': 13, '12': 14, '14': 15, '16': 16, '17': 17, '19': 18, '20': 19, '22': 20, '23': 21, '25': 22, '27': 23, '28': 24, '30': 25, '31': 26, '33': 27, '34': 28, '36': 29, '37': 30, '39': 31, '41': 32, '42': 33, '44': 34, '45': 35, '47': 36, '48': 37, '50': 38, '52': 39, '53': 40, '55': 41, '56': 42, '58': 43, '59': 44, '61': 45, '62': 46, '64': 47, '66': 48, '67': 49, '69': 50, '70': 51, '72': 52, '73': 53, '75': 54, '77': 55, '78': 56, '80': 57, '81': 58, '83': 59, '84': 60, '86': 61, '87': 62, '89': 63, '91': 64, '92': 65, '94': 66, '95': 67, '97': 68, '98': 69, '100': 70 };
+var bi6_to_h264_CRF_map = { '0': 48, '1': 48, '2': 47, '3': 47, '4': 47, '5': 46, '6': 46, '7': 46, '8': 45, '9': 45, '10': 45, '11': 44, '12': 44, '13': 44, '14': 44, '15': 43, '16': 43, '17': 43, '18': 42, '19': 42, '20': 42, '21': 41, '22': 41, '23': 41, '24': 40, '25': 40, '26': 40, '27': 39, '28': 39, '29': 39, '30': 38, '31': 38, '32': 38, '33': 37, '34': 37, '35': 37, '36': 36, '37': 36, '38': 36, '39': 36, '40': 35, '41': 35, '42': 35, '43': 34, '44': 34, '45': 34, '46': 33, '47': 33, '48': 33, '49': 32, '50': 32, '51': 32, '52': 31, '53': 31, '54': 31, '55': 30, '56': 30, '57': 30, '58': 29, '59': 29, '60': 29, '61': 28, '62': 28, '63': 28, '64': 28, '65': 27, '66': 27, '67': 27, '68': 26, '69': 26, '70': 26, '71': 25, '72': 25, '73': 25, '74': 24, '75': 24, '76': 24, '77': 23, '78': 23, '79': 23, '80': 22, '81': 22, '82': 22, '83': 21, '84': 21, '85': 21, '86': 20, '87': 20, '88': 20, '89': 20, '90': 19, '91': 19, '92': 19, '93': 18, '94': 18, '95': 18, '96': 17, '97': 17, '98': 17, '99': 16, '100': 16 };
+var bi6_to_h265_CRF_map = { '0': 51, '1': 51, '2': 50, '3': 50, '4': 50, '5': 49, '6': 49, '7': 49, '8': 48, '9': 48, '10': 48, '11': 47, '12': 47, '13': 47, '14': 47, '15': 46, '16': 46, '17': 46, '18': 45, '19': 45, '20': 45, '21': 44, '22': 44, '23': 44, '24': 43, '25': 43, '26': 43, '27': 42, '28': 42, '29': 42, '30': 41, '31': 41, '32': 41, '33': 40, '34': 40, '35': 40, '36': 39, '37': 39, '38': 39, '39': 39, '40': 38, '41': 38, '42': 38, '43': 37, '44': 37, '45': 37, '46': 36, '47': 36, '48': 36, '49': 35, '50': 35, '51': 35, '52': 34, '53': 34, '54': 34, '55': 33, '56': 33, '57': 33, '58': 32, '59': 32, '60': 32, '61': 31, '62': 31, '63': 31, '64': 31, '65': 30, '66': 30, '67': 30, '68': 29, '69': 29, '70': 29, '71': 28, '72': 28, '73': 28, '74': 27, '75': 27, '76': 27, '77': 26, '78': 26, '79': 26, '80': 25, '81': 25, '82': 25, '83': 24, '84': 24, '85': 24, '86': 23, '87': 23, '88': 23, '89': 23, '90': 22, '91': 22, '92': 22, '93': 21, '94': 21, '95': 21, '96': 20, '97': 20, '98': 20, '99': 19, '100': 19 };
+
 ///////////////////////////////////////////////////////////////
 // Group Layout Dialog ////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
@@ -36155,9 +36228,13 @@ function MakeAddEditorFieldFn(title, $content, obj, o)
 					fieldArgs.value = options.options[value];
 			}
 		}
-		else if (type === "comment")
+		else if (type === "comment" || type === "commentText")
 		{
 			fieldArgs.inputType = "commentText";
+		}
+		else if (type === "commentHtml")
+		{
+			fieldArgs.inputType = "commentHtml";
 		}
 		else if (type === "errorCommentText")
 		{
